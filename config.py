14a853ca5e09ed27a8c759dbccea9282272e47d8
zyf <shenzhen2625@outlook.com>
2026-06-03 14:05:24 +0800

    commit

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # DashScope API
-DASHSCOPE_API_KEY = "sk-c4918b38a03e43ce92d7e6bdaf278a23"
+DASHSCOPE_API_KEY = ""
 
 # Embedding API (DashScope embedding)
 EMBEDDING_MODEL = "tongyi-embedding-vision-plus-2026-03-06"
